960addb7c940f8356ea43ebd590330d5bd4b111c
harshul79 <95867543+harshul79@users.noreply.github.com>
2022-01-14 13:39:51 +0530

    Add files via upload

---
+++ harshul.py
@@ -0,0 +1,8 @@
+a=int(input("enter first number"))
+b=int(input("enter your second number"))
+if a>b:
+          print("a is big")
+elif a==b:
+          print("a and b are equal")
+else:
+          print("b is big")
